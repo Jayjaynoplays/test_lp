6aa6f7931e3fe101ce96476b7986442b2227c228
Jayjaynoplays <nhenh2712@gmail.com>
2021-02-14 18:31:58 +0700

    done 80% navbar responsive

--- scripts/home.js
+++ scripts/home.js
@@ -1,15 +1,23 @@
-let btns = document.querySelectorAll('.btn--home')
-btns.forEach(btn => {
-    btn.onclick = (e) =>{
-        let x = e.clientX - e.target.offsetLeft
-        let y = e.clientY - e.target.offsetTop - 80
-        let circle = document.createElement('span')
-        circle.style.left = x + 'px'
-        circle.style.top = y  + 'px'
-        btn.appendChild(circle);
-        setTimeout(()=>{
-            circle.remove()
-        },1000)
-    }
-})
+let btnsHome = document.querySelectorAll('.btn--home')
+let btnsNav = document.querySelectorAll('.btn--navbar')
+
+function circleBlurAnimation(btns, constant) {
+    btns.forEach(btn => {
+        btn.onclick = (e) => {
+            let x = e.clientX - e.target.offsetLeft
+            let y = e.clientY - e.target.offsetTop - constant
+            let circle = document.createElement('span')
+            circle.style.left = x + 'px'
+            circle.style.top = y + 'px'
+            btn.appendChild(circle);
+            setTimeout(() => {
+                circle.remove()
+            }, 1000)
+        }
+    })
+}
+
+circleBlurAnimation(btnsHome, 80);
+circleBlurAnimation(btnsNav,0);
+
 

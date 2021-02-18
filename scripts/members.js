d881769f85c009faa965cbf02551ce959b91f08d
Jayjaynoplays <nhenh2712@gmail.com>
2021-02-19 00:24:41 +0700

    done responsive  members section

--- scripts/members.js
+++ scripts/members.js
@@ -1,7 +1,6 @@
 var width = $(window).width();
 var rowArr = document.querySelectorAll('.row')
-console.log(rowArr);
 if(767.98 <= width <= 991.98){
     rowArr[1].classList.add('memberPhoto__768');
     rowArr[2].classList.add('memberPhoto__768');
-}
+};

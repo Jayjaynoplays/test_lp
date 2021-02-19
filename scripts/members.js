ad60caddd9697a8051955a0cec036313c9bcddd1
Jayjaynoplays <nhenh2712@gmail.com>
2021-02-19 23:33:23 +0700

    deploy lp test 2 repo

--- scripts/members.js
+++ scripts/members.js
@@ -3,4 +3,4 @@ var rowArr = document.querySelectorAll('.row')
 if(767.98 <= width <= 991.98){
     rowArr[1].classList.add('memberPhoto__768');
     rowArr[2].classList.add('memberPhoto__768');
-};
+}
